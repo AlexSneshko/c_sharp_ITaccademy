327b5e7e64824538be8ff773ea50327ae5d628f2
Alex Snetko <007alex03@gmail.com>
2021-07-19 00:58:05 +0300

    Add project files.

---
+++ tester/tester/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace tester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nailsAmount, hitsAmount;
+            Console.Write("Print amount of nail: ");
+            nailsAmount = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Amount of hits: ");
+            hitsAmount = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            int nails = nailsAmount;
+            while (nails > 0)
+            {
+                for(int i = 0; i < hitsAmount; i++)
+                    Console.WriteLine("Hit");
+                Console.WriteLine("{0} nail done\n", nailsAmount - nails + 1);
+                nails--;
+            }
+        }
+    }
+}
