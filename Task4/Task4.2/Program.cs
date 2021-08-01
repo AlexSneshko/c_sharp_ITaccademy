ca4beb4cf76bb141000d2904033e581d0e3e2b00
Alex Snetko <007alex03@gmail.com>
2021-08-02 02:12:02 +0300

    done

---
+++ Task4/Task4.2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = 201, finish = 500;
+
+            while(start < finish)
+            {
+                if (start % 17 == 0)
+                    Console.Write("{0}, ", start);
+                start++;
+            }
+        }
+    }
+}
---
+++ Task4/Task4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Print number: ");
+            int a = Int32.Parse(Console.ReadLine());
+
+            if (a >= 0 && a <= 30)
+            {
+                Console.WriteLine("1st interval 0-30");
+            } else if (a >= 31 && a <= 60)
+            {
+                Console.WriteLine("2nd interval 31-60");
+            }
+            else if (a >= 61 && a <= 100)
+            {
+                Console.WriteLine("2nd interval 61-100");
+            }
+            else
+            {
+                Console.WriteLine("Unidentified number");
+            }
+        }
+    }
+}
