4f54deb66990193607cc11543895b3eac1a81c66
Alex Snetko <007alex03@gmail.com>
2021-08-08 21:43:54 +0300

    fixed

--- Task5/Task5/Program.cs
+++ Task5/Task5/Program.cs
@@ -3,33 +3,6 @@ using ClassLibrary;
 
 namespace Task5
 {
-    //class ArrayWorker
-    //{
-    //    public static int[] Reverse(int[] input)
-    //    {
-    //        Array.Reverse(input);
-    //        return input;
-    //    }
-
-    //    public static void ToString(int[] input)
-    //    {
-    //        for (int i = 0; i < input.Length; i++)
-    //            Console.Write("{0}, ", input[i]);
-    //        Console.WriteLine();
-    //    }
-
-    //    public static void Add(ref int[] source, int index, int newValue)
-    //    {
-    //        Array.Resize<int>(ref source, source.Length + 1);
-
-    //        for (int i = source.Length - 1; i > index; i--)
-    //            source[i] = source[i - 1];
-    //        source[index] = newValue;
-    //    }
-    //}
-
-   
-
     class Program
     {
 
