a47861239dc49c73550aa9fb9b0da446947de2f3
Alex Snetko <007alex03@gmail.com>
2021-08-09 17:59:01 +0300

    done

---
+++ Task7/Task7/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Task7
+{
+    class Program
+    {
+        static void program1()
+        {
+            var array = new int[6];
+            Console.WriteLine("Print a numbers: ");
+            for (int i = 0; i < array.Length; i++)
+                array[i] = int.Parse(Console.ReadLine());
+
+            Array.Sort(array);
+            Array.Reverse(array);
+
+            foreach (var number in array)
+                Console.Write($"{number} ");
+        }
+
+        static void program2()
+        { 
+
+            var array = new int[3, 3] { { 2, 7, 8 }, { 4, 1, 2 }, { 5, 6, 0 } };
+
+            for(int i = 0; i <= array.GetUpperBound(0); i++)
+            {
+                int max = array[i, 0];
+
+                for (int j = 0; j < array.Length / (array.GetUpperBound(0) + 1); j++)
+                {
+                    if (array[i, j] > max)
+                        max = array[i, j];
+                }
+
+                Console.WriteLine($"Max in {i + 1} column: {max}");
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            //program1();
+            program2();
+        }
+    }
+}
